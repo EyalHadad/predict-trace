203919f332a8b004723f5d26488aedfbf5ae2f57
Eyal Hadad <eyalhadad1989@gmail.com>
2019-08-12 18:14:03 +0300

    copy traces function

--- additional_functions.py
+++ additional_functions.py
@@ -3,6 +3,7 @@ import io
 import os
 import copy
 import random
+import shutil
 import time
 import pandas as pd
 
@@ -104,20 +105,20 @@ def insert_values_to_dict(dict_results, value_list):
 
 
 def get_string_from_dict(dict_result):
-    auc = dict_result['auc']/dict_result['Version_count']
-    acc = dict_result['acc']/dict_result['Version_count']
-    tn = (dict_result['Tn']/dict_result['Total'])*100
-    fn = (dict_result['Fn']/dict_result['Total'])*100
-    tp = (dict_result['Tp']/dict_result['Total'])*100
-    fp = (dict_result['Fp']/dict_result['Total'])*100
-    return str(auc) + "," + str(acc) + "," + str(tn) + "%," + str(fp) + "%," + str(fn) + "%," + str(tp)+"%\n"
+    auc = dict_result['auc'] / dict_result['Version_count']
+    acc = dict_result['acc'] / dict_result['Version_count']
+    tn = (dict_result['Tn'] / dict_result['Total']) * 100
+    fn = (dict_result['Fn'] / dict_result['Total']) * 100
+    tp = (dict_result['Tp'] / dict_result['Total']) * 100
+    fp = (dict_result['Fp'] / dict_result['Total']) * 100
+    return str(auc) + "," + str(acc) + "," + str(tn) + "%," + str(fp) + "%," + str(fn) + "%," + str(tp) + "%\n"
 
 
 def calculate_prediction_results(version_path):
     learning_results_file_list = get_learning_results_files(version_path)
-    nn_dict_results = {'acc':0,'auc':0,'Tn':0,'Fn':0,'Tp':0,'Fp':0,'Total':0,'Version_count':0}
-    dnn_dict_results= {'acc':0,'auc':0,'Tn':0,'Fn':0,'Tp':0,'Fp':0,'Total':0,'Version_count':0}
-    xgb_dict_results= {'acc':0,'auc':0,'Tn':0,'Fn':0,'Tp':0,'Fp':0,'Total':0,'Version_count':0}
+    nn_dict_results = {'acc': 0, 'auc': 0, 'Tn': 0, 'Fn': 0, 'Tp': 0, 'Fp': 0, 'Total': 0, 'Version_count': 0}
+    dnn_dict_results = {'acc': 0, 'auc': 0, 'Tn': 0, 'Fn': 0, 'Tp': 0, 'Fp': 0, 'Total': 0, 'Version_count': 0}
+    xgb_dict_results = {'acc': 0, 'auc': 0, 'Tn': 0, 'Fn': 0, 'Tp': 0, 'Fp': 0, 'Total': 0, 'Version_count': 0}
     # result_summary = old_sum_results(result)
     for res_file in learning_results_file_list:
         with open(res_file, "r") as f:
@@ -134,7 +135,7 @@ def calculate_prediction_results(version_path):
     nn_s = get_string_from_dict(nn_dict_results)
     dnn_s = get_string_from_dict(dnn_dict_results)
     xgb_s = get_string_from_dict(xgb_dict_results)
-    with open(os.path.join(version_path,"sum_learning_res.csv"), "w") as f:
+    with open(os.path.join(version_path, "sum_learning_res.csv"), "w") as f:
         f.write("Algorithm,AUC,Acc,TN,FP,FN,TP\n")
         f.write("NN," + nn_s)
         f.write("DNN," + dnn_s)
@@ -166,10 +167,27 @@ def find_prev_classifier_version(ADDITIONAL_FILES_PATH, bug_id):
     return [os.path.join(ADDITIONAL_FILES_PATH, f) for f in os.listdir(tmp_add_file) if f.endswith('.pkl')]
 
 
+def copy_traces(source_path, dest_path):
+    copy_conter = 0
+    for num in range(141):
+        new_source_path = source_path.replace("@", str(num))
+        new_dest_path = dest_path.replace("@", str(num))
+        if os.path.isdir(new_source_path) and os.path.isdir(new_dest_path):
+            file_to_move = os.path.join(new_source_path, 'traceFile.txt')
+            if os.path.exists(file_to_move):
+                shutil.copy(file_to_move, new_dest_path)
+                print(new_dest_path)
+                copy_conter = copy_conter + 1
+
+    print(str(copy_conter) + " file were copied")
+
+
 if __name__ == '__main__':
     # prediction_input_file = r'C:\Users\eyalhad\Desktop\runningProjects\Math_version\math_2_fix\additionalFiles\predictionInputToNN.csv'
     # partial_predicted_data(prediction_input_file)
-    calculate_prediction_results(r'C:\Users\eyalhad\Desktop\runningProjects\Lang_version')
+    # calculate_prediction_results(r'C:\Users\eyalhad\Desktop\runningProjects\Lang_version')
+    copy_traces(r'C:\Users\eyalhad\Desktop\copyTrace\Lang\lang_@_fix',
+                r'C:\Users\eyalhad\Desktop\runningProjects\Lang_version\lang_@_fix\additionalFiles')
     # diagnose_summary_results(r'C:\Users\eyalhad\Desktop\runningProjects\Lang_version\results.csv', r'C:\Users\eyalhad\Desktop\runningProjects\Lang_version\results_sum.csv')
     # print(find_prev_classifier_version(r'C:\Users\eyalhad\Desktop\runningProjects\Math_version\math_6_fix
     # \additionalFiles', '6'))
